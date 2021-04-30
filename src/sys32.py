aae50fd6afb1d7908034fd67638c78b36129b10d
FuchsCrafter <69680700+FuchsCrafter@users.noreply.github.com>
2021-04-30 20:06:43 +0200

    Add files via upload

---
+++ src/sys32.py
@@ -0,0 +1,10 @@
+# ScratchOS system module.
+# Copyright 2021 fuchscrafter.de, GNU GPL 3 license, support(at)fuchscrafter.de
+# DONT TUCH ANYTHING IF YOU DONT KNOW EXACTLY WAHT YOU ARE DOING!
+
+import os, sys
+import sqlite3
+import time
+
+def helloworld():
+    print("Hello World!")
